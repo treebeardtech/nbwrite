d9e6de7c52dc83e3c8187722461f6e64f711ea6e
alex-treebeard <alex@treebeard.io>
2023-10-10 10:25:39 +0000

    setup

--- src/nbwrite/ex.py
+++ src/nbwrite/ex.py
@@ -1,3 +1,20 @@
+from langchain import PromptTemplate
 
-def blah():
-  return 1
+template = """Question: {question}
+
+Answer: """
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# user question
+question = "Which NFL team won the Super Bowl in the 2010 season?"
+
+import os
+
+from langchain.llms import OpenAI
+
+davinci = OpenAI(model_name="text-davinci-003")
+from langchain import HuggingFaceHub, LLMChain
+
+llm_chain = LLMChain(prompt=prompt, llm=davinci)
+
+print(llm_chain.run(question))
--- tests/test_ex.py
+++ tests/test_ex.py
@@ -1,4 +1,11 @@
+import nbmake
+
 from nbwrite import ex
 
-def test_ex():
-  assert ex.blah() == 1
+
+def test_when_index_then_build_index():
+    assert ex.question == "Which NFL team won the Super Bowl in the 2010 season?"
+
+
+def test_when_gen_nbrun_doc_then_nb_output():
+    assert ex.question == "Which NFL team won the Super Bowl in the 2010 season?"
