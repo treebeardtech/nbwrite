ac4bf6887d9de7ac07d1df0469ea721f2b2157e2
alex-treebeard <alex@treebeard.io>
2023-10-10 11:50:34 +0000

    openai example

--- src/nbwrite/ex.py
+++ src/nbwrite/ex.py
@@ -1,35 +1,35 @@
-from langchain import PromptTemplate
+# from langchain import PromptTemplate
 
-template = """Question: {question}
+# template = """Question: {question}
 
-Answer: """
-prompt = PromptTemplate(template=template, input_variables=["question"])
+# Answer: """
+# prompt = PromptTemplate(template=template, input_variables=["question"])
 
-# user question
+# # user question
 question = "Which NFL team won the Super Bowl in the 2010 season?"
 
-import os
+# import os
 
-from langchain.llms import OpenAI
+# from langchain.llms import OpenAI
 
-davinci = OpenAI(model_name="text-davinci-003")
-from langchain import HuggingFaceHub, LLMChain
+# davinci = OpenAI(model_name="text-davinci-003")
+# from langchain import HuggingFaceHub, LLMChain
 
-llm_chain = LLMChain(prompt=prompt, llm=davinci)
+# llm_chain = LLMChain(prompt=prompt, llm=davinci)
 
-print(llm_chain.run(question))
+# print(llm_chain.run(question))
 
-import pinecone
+# import pinecone
 
-pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="gcp-starter")
-index = pinecone.Index("nbwrite")
+# pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="gcp-starter")
+# index = pinecone.Index("nbwrite")
 
-index.upsert(
-    [
-        ("A", [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
-        ("B", [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
-        ("C", [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]),
-        ("D", [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
-        ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]),
-    ]
-)
+# index.upsert(
+#     [
+#         ("A", [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
+#         ("B", [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
+#         ("C", [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]),
+#         ("D", [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
+#         ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]),
+#     ]
+# )
