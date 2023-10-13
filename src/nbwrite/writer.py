1e2b0d1aa406f43ee37f80af1175f4f6a29d009a
alex-treebeard <alex@treebeard.io>
2023-10-13 11:39:09 +0000

    doc context

--- src/nbwrite/writer.py
+++ src/nbwrite/writer.py
@@ -1,14 +1,27 @@
 import datetime
+import importlib.util
 import os
 from pathlib import Path
 from typing import Any, Dict, List
 
 from langchain.callbacks import wandb_tracing_enabled as _
-from langchain.llms import OpenAI
+from langchain.llms.openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage
 from nbformat import write
-from nbformat.v4 import new_code_cell, new_markdown_cell, new_notebook
+from nbformat.v4 import (
+    new_code_cell,
+    new_markdown_cell,
+    new_notebook,
+    reads,
+    writes,
+)
+
+lib = importlib.util.find_spec("nbmake")
+
+# Assumes the 'openai-python' repository exists in the user's root directory
+code_root = Path(lib.submodule_search_locations._path[0])  # type: ignore
+code = (code_root / "nb_run.py").read_text()
 
 
 def complete(path: Path, out_path: Path):
@@ -16,12 +29,14 @@ def complete(path: Path, out_path: Path):
     # openai.organization = os.getenv("OPENAI_ORG_ID")
     temperature = 0.7
     llm = OpenAI(temperature=temperature)
-    task = "reverse a list"
+    task = (
+        "use nbmake's NotebookRun class to test 'res/x.ipynb' from a Python application"
+    )
 
     prompt = PromptTemplate.from_template(
-        "Write a short Python snippet (max 15 lines) that implements {task}?"
+        "Write an executable Python example in under 50 lines that implements '{task}'.\n\nUse this relevant dependency:\n\n{code}"
     )
-    messages = [HumanMessage(content=prompt.format(task=task))]
+    messages = [HumanMessage(content=prompt.format(task=task, code=code))]
     code_out = llm.predict_messages(messages)
 
     description_prompt = PromptTemplate.from_template(
@@ -38,4 +53,8 @@ def complete(path: Path, out_path: Path):
     nb.cells.append(new_markdown_cell(f"# {title}\n\n{description_out.content}"))
     for src in sources:
         nb.cells.append(new_code_cell(src))
+
+    # double check some basic structure
+    string = writes(nb)
+    _ = reads(string)
     write(nb, str(out_path))
